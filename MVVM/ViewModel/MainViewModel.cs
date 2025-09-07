b7a486e7a386f53adf6ab8a109eccbf19c37a253
VidyakinIvan <D.V4an@yandex.ru>
2025-09-07 23:29:24 +0300

    Add windows closing logic

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Modding_Assistant.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Modding_Assistant.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private RelayCommand? minimizeCommand;
+        private RelayCommand? exitCommand;
+        
+        public RelayCommand? ExitCommand
+        {
+            get
+            {
+                return minimizeCommand ??= new RelayCommand(obj =>
+                {
+                    Application.Current.Shutdown();
+                });
+            }
+        }
+    }
+}
