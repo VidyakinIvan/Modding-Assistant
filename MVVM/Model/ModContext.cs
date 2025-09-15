a64b49641a15ceb85ffe0e8717c02786e7af3db1
VidyakinIvan <D.V4an@yandex.ru>
2025-09-15 13:35:49 +0300

    Fix db connection hardcode

--- MVVM/Model/ModContext.cs
+++ MVVM/Model/ModContext.cs
@@ -2,13 +2,8 @@
 
 namespace Modding_Assistant.MVVM.Model
 {
-    public class ModContext : DbContext
+    public class ModContext(DbContextOptions<ModContext> options) : DbContext(options)
     {
         public DbSet<ModModel> Mods { get; set; }
-        public ModContext() => Database.EnsureCreated();
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=modding_assistant.db");
-        }
     }
 }
