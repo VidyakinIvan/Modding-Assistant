24f08e0121faf5ce8b8b750b128b134a2c9f6f22
VidyakinIvan <D.V4an@yandex.ru>
2025-10-07 13:53:11 +0300

    Refactor window state to margin converter

--- MVVM/View/Converters/WindowStateToMarginConverter.cs
+++ MVVM/View/Converters/WindowStateToMarginConverter.cs
@@ -1,27 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
 namespace Modding_Assistant.MVVM.View.Converters
 {
+    /// <summary>
+    /// Converts <see cref="WindowState"/> to <see cref="Thickness"/> margins to compensate for window chrome differences
+    /// </summary>
+    /// <remarks>
+    /// This converter is specifically designed to handle the visual differences between window states.
+    /// When a window is maximized, it typically loses its window chrome (border, title bar shadows, etc.),
+    /// which can cause content to appear too close to the screen edges.
+    /// </remarks>
     public class WindowStateToMarginConverter : IValueConverter
     {
+        public Thickness MaximizedMargin { get; set; } = new Thickness(0, 6, 6, 0);
+        public Thickness NormalMargin { get; set; } = new Thickness(0);
+
+        /// <summary>
+        /// Converts a <see cref="WindowState"/> value to a <see cref="Thickness"/> margin
+        /// </summary>
+        /// <returns>
+        /// <see cref="Thickness"/> with margins appropriate for the window state
+        /// </returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is WindowState state)
             {
-                return state == WindowState.Maximized ? new Thickness(0, 6, 6, 0) : new Thickness(0);
+                return state == WindowState.Maximized ? MaximizedMargin : NormalMargin;
             }
             return new Thickness(0);
         }
 
+        /// <summary>
+        /// This method is not implemented as window state to margin conversion is one-way only
+        /// </summary>
+        /// <returns>This method always throws <see cref="NotImplementedException"/></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            => throw new NotImplementedException();
+        {
+            throw new NotImplementedException();
+        }
     }
 }
