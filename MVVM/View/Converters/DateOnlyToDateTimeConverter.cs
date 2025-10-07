90afaca3a9d2e6ffc49ee0e976dab6ead4afceba
VidyakinIvan <D.V4an@yandex.ru>
2025-10-07 11:55:01 +0300

    Refactor date converter

--- MVVM/View/Converters/DateOnlyToDateTimeConverter.cs
+++ MVVM/View/Converters/DateOnlyToDateTimeConverter.cs
@@ -3,8 +3,21 @@ using System.Windows.Data;
 
 namespace Modding_Assistant.MVVM.View.Converters
 {
+    /// <summary>
+    /// Converts between <see cref="DateOnly"/> and <see cref="DateTime"/> types for data binding in WPF
+    /// </summary>
     public class DateOnlyToDateTimeConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a <see cref="DateOnly"/> value to a <see cref="DateTime"/> value
+        /// </summary>
+        /// <returns>
+        /// A <see cref="DateTime"/> value with the same date as the input <see cref="DateOnly"/>
+        /// and time set to <see cref="TimeOnly.MinValue"/>, or null if input is null
+        /// </returns>
+        /// <remarks>
+        /// The conversion preserves the date portion while setting the time to 00:00:00 (midnight).
+        /// </remarks>
         public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is DateOnly dateOnly)
@@ -12,11 +25,22 @@ namespace Modding_Assistant.MVVM.View.Converters
             return null;
         }
 
+        /// <summary>
+        /// Converts a <see cref="DateTime"/> value back to a <see cref="DateOnly"/> value
+        /// </summary>
+        /// <returns>
+        /// A <see cref="DateOnly"/> value representing the date portion of the input <see cref="DateTime"/>,
+        /// or null if input is null
+        /// </returns>
+        /// <remarks>
+        /// The conversion extracts only the date portion from the <see cref="DateTime"/> value,
+        /// effectively discarding the time component.
+        /// </remarks>
         public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is DateTime dateTime)
                 return DateOnly.FromDateTime(dateTime);
-            return default(DateOnly);
+            return null;
         }
     }
 }
