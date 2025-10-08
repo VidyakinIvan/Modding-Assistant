2201adbd5456069bddc02e8d0f614f01c6ed1f14
VidyakinIvan <D.V4an@yandex.ru>
2025-10-08 17:42:37 +0300

    Refactor localizationManager interface

--- MVVM/Services/Interfaces/ILocalizationManagerService.cs
+++ MVVM/Services/Interfaces/ILocalizationManagerService.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Modding_Assistant.MVVM.Services.Interfaces
 {
+    /// <summary>
+    /// Service for managing application localization, including retrieving the current language, 
+    /// switching between available languages, and notifying when the language changes
+    /// </summary>
     public interface ILocalizationManagerService
     {
+        /// <summary>
+        /// Gets the current language setting of the application.
+        /// </summary>
         string CurrentLanguage { get; }
+
+        /// <summary>
+        /// Switches the application to the next available language in the predefined language sequence
+        /// </summary>
+        /// <remarks>
+        /// This method cycles through the supported languages in a sequential order. When the
+        /// last language in the sequence is reached, it wraps around to the first language
+        /// </remarks>
         void SwitchToNextLanguage();
+
+        /// <summary>
+        /// Occurs when the application's language setting changes
+        /// </summary>
         event PropertyChangedEventHandler? LanguageChanged;
     }
 }
